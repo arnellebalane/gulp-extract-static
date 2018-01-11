85f7e18097461fe3928fa5a6fbf3a9f3f546f8ba
Arnelle Balane <arnellebalane@gmail.com>
2018-01-11 14:43:32 +0800

    fix: Fix typos, format code properly

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ const vinyl = require('vinyl-fs');
 module.exports = (options={}) => {
     return through2.obj(function(chunk, encoding, callback) {
         const contents = chunk.contents.toString();
+        const noop = match => match;
 
         const pattern = options.pattern;
-        const transform = options.transform || match => match;
+        const transform = options.transform || noop;
         const baseUrl = options.baseUrl;
 
         const matches = [];
 
         let match;
         while (match = pattern.exec(contents)) {
-            const tranformed = transform(match, options);
-            matches.push(transformed);
+            const transformed = transform(match, options);
+            if (transformed) {
+                matches.push(transformed);
+            }
         }
 
-        vinylfs.src(resources, { base: baseUrl }).pipe(through2.obj((chunk, encoding, cb) => {
+        vinyl.src(matches, {
+            base: baseUrl,
+            allowEmpty: true
+        }).pipe(through2.obj((chunk, encoding, cb) => {
             this.push(chunk);
             cb(null);
         }, () => callback(null)));
